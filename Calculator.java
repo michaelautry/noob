e021a2c558423c4144f24fc3abe016f24b0c9a6e
michaelautry <michael.d.autry@gmail.com>
2015-09-03 16:42:42 -0400

    Create Calculator.java
    Edits to be made:
    1. The following libraries were imported but never used:
    import java.io.BufferedReader;
    import java.io.InputStreamReader;
    2. Your Sanner was never closed after use:
    Scanner scannerObject = new Scanner(System.in);

---
+++ Calculator.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Michael Autry's Calculator Code
+ * For UMUC CMIS 141 6381 - Professor: David Brownell
+ * Copyright 2015
+ */
+ 
+public class Calculator 
+{	
+    public static void main(String[] args) throws IOException
+     { 
+	int i1, i2;
+	String operation;
+	Scanner scannerObject = new Scanner(System.in);
+	
+	System.out.println("Please enter your first integer.");
+	i1 = scannerObject. nextInt();
+	
+	System.out.println("Please enter your second integer.");
+	i2 = scannerObject. nextInt();
+	
+	Scanner op = new Scanner(System.in);
+        System.out.println("Please enter your operation ( +, -, /, %, *, >>, <<, &, |).");
+        operation = op.next();
+		
+		switch (operation)  {
+        case "+":
+            System.out.println("The result of "+ i1 + " + " + i2 + " is " + (i1 + i2) + ".");
+			break;
+			
+        case "-":
+            System.out.println("The result of "+ i1 + " - " + i2 + " is " + (i1 - i2) + ".");
+            break;
+
+        case "/":
+            System.out.println("The result of "+ i1 + " / " + i2 + " is " + (i1 / i2) + ".");
+            break;
+			
+        case "%":
+            System.out.println("The result of "+ i1 + " % " + i2 + " is " + (i1 % i2) + ".");
+			break;
+		
+        case "*":
+            System.out.println("The result of "+ i1 + " * " + i2 + " is " + (i1 * i2) + ".");
+			break;
+			
+        case ">>":
+            System.out.println("The result of "+ i1 + " >> " + i2 + " is " + (i1 >> i2) + ".");
+			break;
+			
+        case "<<":
+            System.out.println("The result of "+ i1 + " << " + i2 + " is " + (i1 << i2) + ".");
+			break;
+				
+        case "&":
+            System.out.println("The result of "+ i1 + " & " + i2 + " is " + (i1 & i2) + ".");
+			break;
+			
+        case "|":
+            System.out.println("The result of "+ i1 + " | " + i2 + " is " + (i1 | i2) + ".");
+			break;
+			
+		default:
+			System.out.print("Please try again.");
+			break;
+		
+		}
+	
+	}
+	// Thanks for the challenge!
+}
