43be9329e372585ae9a6a7e1d2b9bed6514f82e1
michaelautry <michael.d.autry@gmail.com>
2015-09-03 16:40:29 -0400

    Create Playgame.java

---
+++ Playgame.java
@@ -0,0 +1,11 @@
+/**
+* Print "Shall we play a game?" to standard output.
+*/
+public class playgame
+  {
+  public static void main(String[] args)
+  {
+// Display the string.
+  System.out.println("Shall we play a game?");
+  }
+}
