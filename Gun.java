29146ba6f7bdfe99cc434fe99a78cfd89048532a
michaelautry <michael.d.autry@gmail.com>
2015-09-11 12:15:58 -0400

    Update Gun.java

--- Gun.java
+++ Gun.java
@@ -1,41 +1,35 @@
 public class Gun {
-    // the Gun class has    
-    // four fields    
-    private int damage;    
-    private int magazine;         
-    private int range;    
-    private int rounds;   
-    
-    // the Gun class has    
-    // one constructor    
-    public Gun(int changeDamage, int changeMagazine, int changeRange) {        
-        damage = changeDamage;        
-        magazine = changeMagazine;        
-        range = changeRange;    
         
-    }   
-    
-    // the Gun class has    
+    // the Gun class has
+    // three fields
+    private int damage;
+    private int magazine;
+    private int rounds;
+        
+    // the Gun class has
+    // one constructor
+    public Gun(int startdamage, int startrounds, int startmagazine) {
+        magazine = startmagazine;
+        damage = startdamage;
+        rounds = startrounds;
+    }
+        
+    // the Gun class has
     // four methods
-    
-    public void changeDamage(int newValue) {        
-        damage = newValue;    
+    public void setdamage(int newValue) {
+        damage = newValue;
+    }
         
-    }            
-    
-    public void changeMagazine(int newValue) {        
-        magazine = newValue;    
-            
-    }            
-    
+    public void setmagazine(int newValue) {
+        magazine = newValue;
+    }
+        
     public void pullTrigger(int decrement) {
-        rounds -= decrement;    
+        rounds -= decrement;
+    }
         
-    }            
-    
     public void reload(int increment) {
-        rounds += increment;    
+        rounds += increment;
+    }
         
-    }        
-    
 }
