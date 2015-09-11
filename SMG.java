9cd017e954679ba954867cb2b6bf7696e3d39f6d
michaelautry <michael.d.autry@gmail.com>
2015-09-10 23:49:19 -0400

    Update SMG.java

--- SMG.java
+++ SMG.java
@@ -1,42 +1,22 @@
 public class SMG extends Gun {
-        
-    // the SMG subclass has
-    // four fields
-    private int magazineSize;
-    private int roundType;
-    private int damage;
-    private int range;
-
 
     // the SMG subclass has
-    // one constructor
-    public SMG() {
-        int magazineSize = 30;
-        int roundType = 9;
-        int damage = 35;
-        int range = 300;
-    }   
-        
+    // one field
+
+public int caliber;
+
     // the SMG subclass has
-    // four methods
-    public void changeMagazineSize(int newValue) {
-      magazineSize = newValue;
-  }
+    // one constructor    
 
-  public void changeRoundType (int newValue) {
-      roundType = newValue;    
-  }
+public SMG(int setCaliber, int changeMagazine, int changeRange) {
+        caliber = setCaliber;
 
-  public void changeDamage (int newValue) {
-      damage = newValue;    
-  }
-
-  public void changeRange (int newValue) {
-      range = newValue;
-  }
-
-  public void printStates() {
-      System.out.println("Magazine size:" + magazineSize + " round:" + roundType + " damage:" + damage + " range:" + range );
-  }
-  
+    }           
+    
+    // the SMG subclass has
+    // one method
+    public void setCaliber(int newValue) {
+        caliber = newValue;
+    }
+    
 }
